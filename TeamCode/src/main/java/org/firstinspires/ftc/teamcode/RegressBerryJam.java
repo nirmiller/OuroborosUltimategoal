8639a5336bd02f21cec75c90b94ffd05780d5c26
TJTheCoder <49538138+TJTheCoder@users.noreply.github.com>
2020-10-25 13:13:21 -0500

    Jam Vex!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RegressBerryJam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RegressBerryJam.java
@@ -130,7 +130,8 @@ public class RegressBerryJam extends LinearOpMode {
                             ((y - verticalRaw) / verticalDeviation); } }
         jar /= index; telemetry.addData("Berry Code: ",
                 "The berry code is " + jar * (verticalDeviation / horizontalDeviation));
-        telemetry.addData("Jam Vex",  verticalRaw - (jar * horizontalRaw));
+        telemetry.addData("Jam Vex: ",
+                "The jam vex is "+ (verticalRaw - (jar * horizontalRaw)));
         telemetry.addData("Success! ", "Success!"); telemetry.update();
         frame.recycle();
     }
