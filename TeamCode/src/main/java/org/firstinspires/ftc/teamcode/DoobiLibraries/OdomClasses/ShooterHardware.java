b448f6124b83eaa4db407320b9540ba26eca975b
nirmiller <52181530+nirmiller@users.noreply.github.com>
2020-12-02 19:16:48 -0600

    Good

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoobiLibraries/OdomClasses/ShooterHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoobiLibraries/OdomClasses/ShooterHardware.java
@@ -6,12 +6,18 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class ShooterHardware {
 
+
+    static double PIVOT_THETA_TO_ENCODER = 0.0;
+    static double LIFT_INDEX_TO_ENCODER = 0.0;
+
     LinearOpMode opMode;
 
     private DcMotor shooter;
     private DcMotor pivot;
     private DcMotor lift;
     private double currentAngle;
+
+    private double currentLiftHeightIndex;
 
     public ShooterHardware(LinearOpMode opMode)
     {
@@ -35,10 +41,43 @@ public class ShooterHardware {
         double angleChange = theta - currentAngle;
 
 
+        //turn on pivot motors until reached equal encoder level-set an level of error
+
 
         currentAngle = theta;
     }
 
+    public void setLift(double heightIndex)
+    {
+
+        double indexChange = heightIndex - currentLiftHeightIndex;
+
+        //move motors until reached height index
+
+        currentLiftHeightIndex = heightIndex;
+
+    }
+
+    public void shoot()
+    {
+        shooter.setPower(1);
+    }
+
+    public void withdraw()
+    {
+        shooter.setPower(0);
+    }
+    public void resetShooter()
+    {
+        double angleChange = 0 - currentAngle;
+        double indexChange = 0 - currentLiftHeightIndex;
+
+        //Move pivotdown
+        //Move Lift Down
 
 
+
+        currentAngle = 0;
+        currentLiftHeightIndex = 0;
+    }
 }
