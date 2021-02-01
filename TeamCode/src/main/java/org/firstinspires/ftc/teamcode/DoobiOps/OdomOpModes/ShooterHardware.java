d03fe9f06b74a0ca38bb259fc597b81dc4653007
nirmiller <52181530+nirmiller@users.noreply.github.com>
2021-02-01 00:31:58 -0600

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoobiOps/OdomOpModes/ShooterHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoobiOps/OdomOpModes/ShooterHardware.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.DoobiLibraries.OdomClasses;
+package org.firstinspires.ftc.teamcode.DoobiOps.OdomOpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoobiOps/OdomOpModes/TrollAuto3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoobiOps/OdomOpModes/TrollAuto3.java
@@ -10,6 +10,7 @@ import org.firstinspires.ftc.teamcode.DoobiLibraries.OdomClasses.JankOdomDriveTr
 import org.firstinspires.ftc.teamcode.DoobiLibraries.OdomClasses.OdomDriveTrain;
 import org.firstinspires.ftc.teamcode.DoobiLibraries.Point;
 import org.firstinspires.ftc.teamcode.DoobiLibraries.Sensors;
+import org.firstinspires.ftc.teamcode.DoobiLibraries.Shooter;
 import org.firstinspires.ftc.teamcode.DoobiLibraries.Wobble;
 import org.firstinspires.ftc.teamcode.DoobiLibraries.jankOdom;
 
@@ -20,12 +21,43 @@ public class TrollAuto3 extends LinearOpMode {
 
     OdomDriveTrain odt;
     Wobble wobble;
+    ShooterHardware sh;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         odt = new OdomDriveTrain(this);
         wobble = new Wobble(this);
+        sh = new ShooterHardware(this);
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                odt.timeMoveForward(4, .5);
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ElapsedTime time = new ElapsedTime();
+                sh.ignite();
+                while (time.milliseconds() < 2000){
+
+                }
+                sh.withdraw();
+            }
+        });
+
+        waitForStart();
+
+        thread1.start();
+        thread2.start();
+
+        thread1.interrupt();
+        thread2.interrupt();
+
+
 
 
 
